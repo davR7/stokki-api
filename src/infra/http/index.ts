c812a5d10c7c0e3ad46451a41c275c8ddc0e9bf6
davidson ratis <davidsonmakarios@gmail.com>
2026-09-10 12:58:58 -0300

    refactor: register not-found.middleware and catch-all.middleware in App

--- src/infra/http/index.ts
+++ src/infra/http/index.ts
@@ -1,6 +1,8 @@
 import cors from "cors";
 import express, { Application, Router } from "express";
 import helmet from "helmet";
+import { CatchAllMiddleware } from "./middlewares/catch-all.middleware";
+import { NotFoundErrorMiddleware } from "./middlewares/not-found.middleware";
 
 export class App {
   private app: Application = express();
@@ -8,6 +10,7 @@ export class App {
   constructor(private router: Router = Router()) {
     this.setupLoaders();
     this.setupRouter();
+    this.setupMiddlewares();
   }
 
   private setupLoaders() {
@@ -20,6 +23,11 @@ export class App {
   private setupRouter() {
     this.app.get("/health", (_req, res) => res.json({ status: "ok" }));
     this.app.use(this.router);
+  }
+
+  private setupMiddlewares() {
+    this.app.use(new NotFoundErrorMiddleware().execute);
+    this.app.use(new CatchAllMiddleware().execute);
   }
 
   listen(port: number) {
