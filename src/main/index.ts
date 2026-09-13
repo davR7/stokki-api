90b4e9cf551a9b43651f84962871e6cfc98c4d74
davidson ratis <davidsonmakarios@gmail.com>
2026-09-13 17:12:31 -0300

    feat: register main router in App

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,12 +1,13 @@
 import { connectDB } from "@/infra/database";
 import { App } from "@/infra/http";
+import { router } from "./routes";
 
 const port = Number(process.env.PORT);
 
 function startServer() {
   try {
     connectDB();
-    const app = new App();
+    const app = new App(router);
     app.listen(port);
   } catch (err) {
     console.log(`Failed server: ${err}`);
