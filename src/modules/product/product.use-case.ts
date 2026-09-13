f9e51dee2604a7eb8bcdfec553c5519bfeb8dd14
davidson ratis <davidsonmakarios@gmail.com>
2026-09-13 16:58:26 -0300

    feat: implement create method for product.use-case

---
+++ src/modules/product/product.use-case.ts
@@ -0,0 +1,45 @@
+import { ConflictError } from "@/shared/error/conflict.error";
+import { NotFoundError } from "@/shared/error/not-found.error";
+import { CategoryRepository } from "../category/category.repository";
+import { Stock } from "../stock/stock.entity";
+import { StockRepository } from "../stock/stock.repository";
+import { ProductInputDto, ProductOutputDto } from "./product.dto";
+import { Product } from "./product.entity";
+import { ProductMapper } from "./product.mapper";
+import { ProductRepository } from "./product.repository";
+
+export class ProductUseCase {
+  constructor(
+    private productRepository: ProductRepository,
+    private categoryRepository: CategoryRepository,
+    private stockRepository: StockRepository,
+  ) {}
+
+  async create(input: ProductInputDto): Promise<ProductOutputDto> {
+    const category = await this.categoryRepository.findById(input.categoryId);
+
+    if (!category) {
+      throw new NotFoundError("Categoria não encontrada", "CATEGORY_ERR_NOT_FOUND");
+    }
+
+    const product = await this.productRepository.findBySku(input.sku);
+
+    if (product) {
+      throw new ConflictError("Produto já cadastrado", "PRODUCT_ERR_CONFLICT");
+    }
+
+    const { stock, ...rest } = input;
+
+    const output = await this.productRepository.create(Product.create(rest));
+
+    await this.stockRepository.create(
+      Stock.create({
+        quantity: stock.quantity,
+        minimumQuantity: stock.minimumQuantity,
+        productId: output.id,
+      }),
+    );
+
+    return ProductMapper.toUseCase(output);
+  }
+}
