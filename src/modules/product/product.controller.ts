bec1bd1c1d7a47656dc06dda73e82702e50205df
davidson ratis <davidsonmakarios@gmail.com>
2026-09-15 14:34:02 -0300

    feat: implement handleList method in product controller

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -6,6 +6,20 @@ export class ProductController {
 
   async handleCreate(req: Request, res: Response) {
     const output = await this.productUseCase.create(req.body);
+
     return res.status(201).json(output);
+  }
+
+  async handleList(req: Request, res: Response) {
+    const { page = 1, limit = 5, search, categoryId } = req.query;
+
+    const output = await this.productUseCase.list({
+      page: Number(page),
+      limit: Number(limit),
+      search: search?.toString(),
+      categoryId: categoryId?.toString(),
+    });
+
+    return res.json(output);
   }
 }
