61ae368d232d3b0b1872fbc1e7a542a0ab14bde9
davidson ratis <davidsonmakarios@gmail.com>
2026-09-15 14:25:23 -0300

    feat: implement findAll method for product repository

--- src/modules/product/product.repository.ts
+++ src/modules/product/product.repository.ts
@@ -1,6 +1,9 @@
+import { Pagination } from "@/infra/repositories/ports/Pagination";
+import { ProductList } from "@/infra/repositories/ports/ProductList";
 import { Product } from "./product.entity";
 
 export interface ProductRepository {
   create(input: Product): Promise<Product>;
   findBySku(sku: string): Promise<Product | null>;
+  findAll(input: Pagination): Promise<ProductList>;
 }
