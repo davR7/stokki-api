8307b745e0e895f085ddcb3bc6f7a53156293a4f
davidson ratis <davidsonmakarios@gmail.com>
2026-09-16 16:16:30 -0300

    feat: register stock update route

--- src/main/routes/product.router.ts
+++ src/main/routes/product.router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { makeProductFactory } from "../factories/product.factory";
+import { makeStockFactory } from "../factories/stock.factory";
 
 export default (router: Router) => {
   router.post("/products", (req, res) => {
@@ -8,5 +9,9 @@ export default (router: Router) => {
 
   router.get("/products", (req, res) => {
     return makeProductFactory().handleList(req, res);
+  });
+
+  router.patch("/products/:productId/stock", (req, res) => {
+    return makeStockFactory().handleUpdate(req, res);
   });
 };
